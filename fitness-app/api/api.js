e4a9dc0379c2dba4bcddfebf86bf2877ce96c6f1
kiokomutunga <kiokomutungapp@gmail.com>
2025-10-15 02:17:57 +0300

    api.js

---
+++ fitness-app/api/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "/api";
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+    headers:{
+        "Content-Type": "application/json",
+    },
+
+
+});
