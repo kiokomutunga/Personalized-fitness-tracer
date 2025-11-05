e8797700815bec6874a1d83e08d4ad1e2d71948f
kiokomutunga <kiokomutungapp@gmail.com>
2025-11-05 17:19:22 +0300

    login screen

--- fitness-app/utils/app.ts
+++ fitness-app/utils/app.ts
@@ -7,3 +7,11 @@ export const API = axios.create({
 export const getAuthToken = async () => {
     // This function would normally retrieve the auth token from secure storage
     return "your-auth-token";
+};
+
+
+export const loginUser = async (email: string, password: string) => {
+    const response = await API.post("/auth/login", { email, password });
+
+    return response.data;
+};
